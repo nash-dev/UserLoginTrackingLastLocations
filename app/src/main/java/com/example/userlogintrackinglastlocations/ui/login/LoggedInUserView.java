a978ac265846645783f3691e73e21d6e7ac48f1b
nash-dev <avinashnirmal1@gmail.com>
2023-01-24 07:22:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/userlogintrackinglastlocations/ui/login/LoggedInUserView.java
@@ -0,0 +1,16 @@
+package com.example.userlogintrackinglastlocations.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
